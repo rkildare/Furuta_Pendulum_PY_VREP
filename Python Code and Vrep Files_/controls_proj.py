80d637540f5604538ea850b34f32663de937ff00
rkildare <55807922+rkildare@users.noreply.github.com>
2021-10-23 22:57:55 -0700

    Updating to actual PID loop
    Implemented an actual PID loop for the pendulum as well as one for motor position so it can hold position without drifting.
    
    I got bored and cobbled this together today so its pretty rough. Will refine soon.

--- Python Code and Vrep Files_/controls_proj.py
+++ Python Code and Vrep Files_/controls_proj.py
@@ -21,44 +21,96 @@ else:
     sys.exit ("could not connect")
     
 #-----Start the Paused Simulation
-err_code = vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot)
 
+err_code = vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot)
+#err_code = vrep.simxSetJointPosition(clientID,j2,0,vrep.simx_opmode_oneshot)
 
 #-----Initialize Joint Handles---------
 err_code,j1 = vrep.simxGetObjectHandle(clientID,"J1",vrep.simx_opmode_blocking)
 err_code,j2 = vrep.simxGetObjectHandle(clientID,"J2",vrep.simx_opmode_blocking)
 
 #-----Function to get the position of joint 2 (the free joint)---------
-def jupdate():
+def updatePendPos():
     err_code,posj2 = vrep.simxGetJointPosition(clientID, j2,vrep.simx_opmode_streaming)
     return posj2
-
-
-#-----PI----
-
-kp = 5#5
-ki = 90#90
-erri = 0
-inv = 0.01
+def updateMotPos():
+    err_code,posj1 = vrep.simxGetJointPosition(clientID, j1,vrep.simx_opmode_streaming)
+    return posj1
+   
+kp = 29#28
+ki = 1.8#1.8
+kd = 0.05#0.01 - 0.02 <---BIG MAYBE
+perr = 0
 target = 0
-    
-    
-while True:
-    po = jupdate() #Get Pendulum Position
+I=0
+inv = 0.001
+strt = 0
 
-    #-----Establish a target - Had trouble with reversing the pendulums momentum without this block
-    if (po<0):
-        target = 0.1
-    elif(po>0):
-        target = -0.1
-    else:
-        target = 0
-    #------
+kp1 = 7#6 - 5 -8
+ki1 = 0.005#0.001 - 0.001
+kd1 = 800#300 - 10
+po1d = 0
+I1 = 0
+
+a = 0
+spe = -5
+poo=0
+
+while True:
     
-    err = target-po #Calculate Error
-    P = err * kp #Calculated value of P
-    I = erri * ki #Calculated value of I
-    erri = erri + inv*err # Keep a running sum of error for the integral function
-    velcoef = (P + I) #Summation of P and I to get the new velocity of joint 1
-    err_code = vrep.simxSetJointTargetVelocity(clientID,j1,velcoef,vrep.simx_opmode_streaming)#Set the velocity of joint 1
-    time.sleep(inv)#Wait a short abount of time
+    po = updatePendPos()
+    ## SWING UP ##
+    #lpo = 0
+#    while (po>0.6 or po<-0.6):
+#        po = updatePendPos()
+#        if abs(poo)>abs(po):
+#            if po<0:
+#                vel = -6#-5
+#            else:
+#                vel = 6#5
+#            err_code = vrep.simxSetJointTargetVelocity(clientID,j1,vel,vrep.simx_opmode_streaming)
+#        poo = po
+        
+        
+    err = target - po
+    #print(err)
+    P = err * kp
+    I = I + ki*err
+    D = kd * (err-perr)/inv
+    velcoef = P + I + D
+    err_codey = vrep.simxSetJointTargetVelocity(clientID,j1,velcoef,vrep.simx_opmode_streaming)
+    if(err_codey != 0 and strt == 10):
+        err_code = vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot)
+        break
+    perr = err
+    #print(po)
+    if (po>1 or po<-1):
+        err_code = vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot)
+        break
+    
+    if(err-perr<0.001 or err-perr>-0.001):# 0.0001 and -0.0001
+        po1 = updateMotPos()
+
+        if((po1-po1d) < 3):
+            D1 = kd1*(po1-po1d)
+            P1 = po1*kp1
+            I1 = I1+ki1*po1
+
+        pid1 = 0.001*(P1+I1+D1)
+        po1d = po1     
+        target = pid1
+
+    else:
+        target = 0 
+    
+    #print(I1)
+    time.sleep(inv)
+    if(strt <10 ):
+        strt = strt+1
+    a = a + 1
+    if (a>10000):
+        if (target == -1):
+            target =1
+        else:
+            target = -1
+        a = 0
